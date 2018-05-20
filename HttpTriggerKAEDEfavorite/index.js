d31e48f02041c07a8238fbd8b2245a5f64675c6c
hayatata <hayatata@live.jp>
2018-05-20 19:54:55 +0900

    受け取ったツイート情報からファボをする処理

--- HttpTriggerKAEDEfavorite/index.js
+++ HttpTriggerKAEDEfavorite/index.js
@@ -1,17 +1,26 @@
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
-    context.done();
+    var Twitter = require('twitter');
+
+    //入力Tweet
+    var inputTweet = req;
+
+    context.log('Create client.');
+    //Twitterクライアントの作成
+    var client = new Twitter({
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    });
+    context.log('Created client!!.');
+
+    context.log('inputTweet.TweetId:' + inputTweet.body.TweetId);
+
+    client.post('favorites/create', {id: inputTweet.body.TweetId}, function(error, tweet, response) {
+        if(error) throw error;
+        console.log(tweets);  // The favorites.
+        console.log(response);  // Raw response object.
+    });
 };
